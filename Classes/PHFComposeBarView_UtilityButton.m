153e9bdc1a87be0bccba585f270531cb8c16bdfa
Philipe Fatio <me@phili.pe>
2013-11-01 17:01:59 +0100

    Animate unhighlighting of utility button

--- Classes/PHFComposeBarView_UtilityButton.m
+++ Classes/PHFComposeBarView_UtilityButton.m
@@ -3,8 +3,17 @@
 @implementation PHFComposeBarView_UtilityButton
 
 - (void)setHighlighted:(BOOL)highlighted {
-    CGFloat alpha = highlighted ? 0.2f : 1.0f;
-    [self setAlpha:alpha];
+    if (highlighted) {
+        [self setAlpha:0.2f];
+    } else {
+        [UIView animateWithDuration:0.2
+                              delay:0
+                            options:UIViewAnimationOptionBeginFromCurrentState
+                         animations:^{
+                             [self setAlpha:1.0f];
+                         }
+                         completion:NULL];
+    }
 }
 
 @end
